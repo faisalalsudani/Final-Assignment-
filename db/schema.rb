ab10af0e023d5520d30672288af7a819b82fdbd5
faisalalsudani <f4desiign@gmail.com>
2017-10-10 14:32:44 +0200

    Add evaluation to students with seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010101004) do
+ActiveRecord::Schema.define(version: 20171010115742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20171010101004) do
     t.boolean "red"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "student_id"
+    t.index ["student_id"], name: "index_evaluations_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -60,5 +62,6 @@ ActiveRecord::Schema.define(version: 20171010101004) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "evaluations", "students"
   add_foreign_key "students", "batches"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 Student.destroy_all
 Batch.destroy_all
+Evaluation.destroy_all
 
 
 batch1 = Batch.create!(name: "Batch #10", startdate: "28-08-2017", enddate: "08-11-2017")
@@ -13,3 +14,7 @@ student5 = Student.create!(first_name: "Cristiano", last_name: "Ronaldo", label:
 student6 = Student.create!(first_name: "Serio", last_name: "Ramos", label: "Green", photo: "no.pic" ,batch: batch2)
 student7 = Student.create!(first_name: "Alvaro", last_name: "Morata", label: "Red", photo: "no.pic" ,batch: batch1)
 student8 = Student.create!(first_name: "Lionel", last_name: "Messi", label: "Green", photo: "no.pic" ,batch: batch2)
+
+evaluation1 = Evaluation.create!(remarks: "Good work!", student: student1, green: true, yellow:false, red:false)
+evaluation2 = Evaluation.create!(remarks: "Not good work!", student: student1)
+evaluation3 = Evaluation.create!(remarks: "Google more!", student: student2)
